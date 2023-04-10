91331fce5ae9d22a7c1f9b99cc8a289d7a24ed72
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-04-10 10:21:14 +0530

    about us content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='container mx-auto'>
+      <div className='text-lg font-bold'>Welcome to our pizza shopping website! We specialize in providing our customers with a wide range of delicious pizzas that are sure to satisfy any craving. Our website is designed to make your shopping experience as easy and convenient as possible, with a variety of pages to explore. Our product page features all of our pizzas, sorted by category, allowing you to easily find your favorite flavors. Once you've made your selections, our cart page makes it simple to review and modify your order before checkout. And if you need help deciding, our detail page provides all the information you need about each pizza. Finally, our home page is the perfect place to start your pizza journey, featuring our latest deals and promotions. By using APIs to fetch data from our backend, we ensure that our website is always up-to-date with the latest pizza offerings. We take pride in providing our customers with a seamless and enjoyable pizza shopping experience, and we can't wait to help you find your perfect pizza!</div>
+    </div>
   )
 }
 
